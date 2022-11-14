259222a5da7956d46dcb018c68d48dadb7076993
aditya-karampudi <aditya.karam3010@gmail.com>
2022-11-14 12:25:15 -0500

    credit_pharmacy

---
+++ notebooks/Model_categories/support_files/constants.py
@@ -0,0 +1,53 @@
+dependent_variable = "transportation_issues"
+
+### Condition
+condition_categories = ['ano', 'ben',
+     'bld',
+ 'brn',
+ 'cad',
+ 'can',
+ 'cer',
+ 'cir',
+ 'dia',
+ 'dig',
+ 'end',
+ 'gus',
+ 'hdz',
+ 'hiv',
+ 'inf',
+ 'inj',
+ 'men',
+ 'mus',
+ 'ner',
+ 'pre',
+ 'rar',
+ 'res',
+ 'rsk',
+ 'skn',
+ 'sns',
+ 'sor',
+ 'trm',
+ 'vco']
+
+
+
+##### Pharm
+drug_categories = {'antibiotics' :['01', '02', '03', '04', '05', '07', '08', '09', '11', '12', '13', '14', '15', '16'],
+                   'immuno' : ['17', '18', '19', '20', '21'],
+                   'hormonal' : ['22', '23', '24', '25', '26', '27', '28', '29', '30'],
+                   'kidney' : ['31', '32', '33', '34', '35', '36', '37', '38', '39', '40'],
+                   'resp': ['41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', 
+                           '54', '55', '56', '57', '58', '59', '60', '61', '62', '64', '65', '66', '67',
+                           '68', '69', '70', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81',
+                           '82', '83', '84', '85', '86', '87', '88', '89', '90', '92', '93', '94', '95',
+                           '96', '97', '98', '99']}
+
+
+rx_other_attributes = ['rx_bh_ind', 'rx_bh_pmpm_ct', 'rx_branded_ind', 'rx_branded_pmpm_ct',
+       'rx_generic_ind', 'rx_generic_pmpm_ct','rx_mail_pmpm_ct','rx_mail_ind','rx_maint_ind', 'rx_maint_pmpm_ct',
+   'rx_otc_ind', 'rx_otc_pmpm_ct', 'rx_overall_pmpm_ct']
+
+
+
+
+
